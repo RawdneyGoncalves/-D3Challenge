016faebaf16be1b14fac04722621eec2f8da7b9a
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-11 13:51:21 -0300

    Método dicionario
    método dicionario para adicionar o numero de mortes e de infectados nos ultimos dias

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const app = express(); 
+const express = require("express");
+const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 //middllers
-app.set('view engine', 'ejs');
-app.get('/', (req,res)=> {
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+covid = [
+  { name: "day1", mortes: 100, infectados: 6 },
+  { name: "day2", mortes: 100, infectados: 6 },
+  { name: "day3", mortes: 100, infectados: 6 },
+  { name: "day4", mortes: 100, infectados: 6 },
+  { name: "day5", mortes: 100, infectados: 6 },
+  { name: "day6", mortes: 100, infectados: 6 },
+  { name: "day7", mortes: 100, infectados: 6 },
+  { name: "day8", mortes: 100, infectados: 6 }
+];
 
-    res.render('index');
-}); 
+const http = require("http");
+const server = http.createServer(function (request, response) {
+  response.setHeader("Acess-Control-Allow-Origin", "*");
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(carros);
+  response.end();
+});
 
-app.listen(PORT, ()=>{ 
-
-    console.log(`Server rodando na porta ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server rodando na porta ${PORT}`);
 });
