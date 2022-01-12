2fac64cbaa821b9cd0a60b64c1e0e54b61e6c874
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-12 02:11:44 -0300

    Calculo Taxa de letalidade

--- server.js
+++ server.js
@@ -1,42 +1,70 @@
 const express = require("express");
 const app = express();
+const db = require("./db.js");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:8080"
+  })
+);
 const PORT = process.env.PORT || 5000;
 //middllers
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
+  db.insertUser(1000, 2000);
   res.render("index");
 });
 
 covidMorteInfectados = [
-  { id: 1, mortes: 4098, infectados:  1130000},
+  { id: 1, mortes: 4098, infectados: 1130000 },
   { id: 2, mortes: 3029, infectados: 868029 },
-  { id: 3, mortes: 5865, infectados: 2520000},
+  { id: 3, mortes: 5865, infectados: 2520000 },
   { id: 4, mortes: 7754, infectados: 2540000 },
   { id: 5, mortes: 7748, infectados: 2510000 },
-  { id: 6, mortes: 6663, infectados: 2520000}
+  { id: 6, mortes: 6663, infectados: 2520000 }
 ];
 
-app.get("/:id", (req, res) => {
-  const {id} = req.params
+app.get("/resultado/:id", (req, res) => {
+  const { id } = req.params;
 
-  const selectedDay = covidMorteInfectados.find(item => item.id === Number(id))
+  const selectedDay = covidMorteInfectados.find(
+    (item) => item.id === Number(id)
+  );
+  let calculo;
+  if (selectedDay) {
+    calculo = (selectedDay.mortes / selectedDay.infectados) * 100;
+  }
+  
+  if (!selectedDay) {
+    return res.send({
+      error: "Data não localizada!"
+    });
+  }
+  return res.json(calculo);
+});
+
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const selectedDay = covidMorteInfectados.find(
+    (item) => item.id === Number(id)
+  );
 
   if (!selectedDay) {
     return res.send({
-      error: 'Data não localizada!'
-    })
+      error: "Data não localizada!"
+    });
   }
 
-  return res.json(selectedDay)
-})
-
-
+  return res.json(selectedDay);
+});
 
 const http = require("http");
 const server = http.createServer(function (request, response) {
   response.setHeader("Acess-Control-Allow-Origin", "*");
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(carros);
+  response.write(covidMorteInfectados);
   response.end();
 });
 
