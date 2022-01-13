f2c4a3ec1401ad3d081ef864b2b7ebdea7e061df
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-13 01:02:43 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,56 +2,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const Controller = require("./controllers/CovidController")
-
+const Controller = require("./controllers/CovidController");
 
 app.use(
   cors({
-    origin: "http://localhost:8080"
+    origin: "http://localhost:5000"
   })
 );
+//crio uma váriavel ambiente caso exista.
 const PORT = process.env.PORT || 5000;
-//middllers
+//engine do html
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+//CALCULO IRF NÃO USAVEL
 covidMorteInfectados = [
-  { id: 1, mortes: 4098, infectados: 1130000, recuperados:123213 },
-  { id: 2, mortes: 3029, infectados: 868029, recuperados: 123213},
+  { id: 1, mortes: 4098, infectados: 1130000, recuperados: 123213 },
+  { id: 2, mortes: 3029, infectados: 868029, recuperados: 123213 },
   { id: 3, mortes: 5865, infectados: 2520000, recuperados: 123123 },
-  { id: 4, mortes: 7754, infectados: 2540000, recuperados:123123 },
-  { id: 5, mortes: 7748, infectados: 2510000, recuperados:12312321 },
-  { id: 6, mortes: 6663, infectados: 2520000, recuperados: 91283321}
+  { id: 4, mortes: 7754, infectados: 2540000, recuperados: 123123 },
+  { id: 5, mortes: 7748, infectados: 2510000, recuperados: 12312321 },
+  { id: 6, mortes: 6663, infectados: 2520000, recuperados: 91283321 }
 ];
 
-app.get("/ddf/:id", Controller.getDay)
-
-
-app.get("/resultado/:id", (req, res) => {
-  const { id } = req.params;
-  
-
-  for(var i = 0; i < id;i++){
-    console.log((covidMorteInfectados[i].mortes / covidMorteInfectados[i].infectados + covidMorteInfectados[i].recuperados) * 100)
-  }
-
-  const selectedDay = covidMorteInfectados.find(
-    (item) => item.id === Number(id)
-  );
-  let calculo;
-  if (selectedDay) {
-    calculo = (selectedDay.mortes / selectedDay.infectados) * 100;
-  }
-  
-  if (!selectedDay) {
-    return res.send({
-      error: "Data não localizada!"
-    });
-  }
-  return res.json(calculo);
-});
+app.get("/ddf/:id", Controller.getDay);
 
 
 app.get("/:id", (req, res) => {
@@ -81,5 +56,3 @@ const server = http.createServer(function (request, response) {
 app.listen(PORT, () => {
   console.log(`Server rodando na porta ${PORT}`);
 });
-
-
