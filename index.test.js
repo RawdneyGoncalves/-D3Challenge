f21b607bbecf119af01ac4e4e22a6b7a6513c234
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-02-21 13:08:03 -0300

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require('./server')
 
 describe('Testar Meu ap server', ()=> {
-it('should get main route', async()=> {
+it('inserir a rota', async()=> {
     const res = await request(app).get('/')
 
     expect(res.body).status(200)
