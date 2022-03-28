a1ccebe46787ce8f256aeab04f89b2c44c8438e5
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-28 13:21:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,19 +14,8 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
-//desativado
-covidMorteInfectados = [
-  { id: 1, mortes: 4098, infectados: 1130000, recuperados: 123213 },
-  { id: 2, mortes: 3029, infectados: 868029, recuperados: 123213 },
-  { id: 3, mortes: 5865, infectados: 2520000, recuperados: 123123 },
-  { id: 4, mortes: 7754, infectados: 2540000, recuperados: 123123 },
-  { id: 5, mortes: 7748, infectados: 2510000, recuperados: 12312321 },
-  { id: 6, mortes: 6663, infectados: 2520000, recuperados: 91283321 }
-];
-
 
 app.get("/ddf/:id", Controller.getDay);
-
 
 app.get("/:id", (req, res) => {
   const { id } = req.params;
